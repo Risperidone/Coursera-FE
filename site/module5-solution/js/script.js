2225fb9b7ce53e0304930e6c9b77f926d378defe
Risperidone <sunnyrainy20158@gmail.com>
2016-12-07 19:05:45 +0800

    GGinin?

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -91,7 +91,7 @@ $ajaxUtils.sendGetRequest(
     function (homeHtml) {
       var chosenCategoryShortName = chooseRandomCategory(categories)["short_name"];
       console.log(chosenCategoryShortName)
-      insertProperty(homeHtml, "randomCategoryShortName", 'chosenCategoryShortName');
+      homeHtml = insertProperty(homeHtml, "randomCategoryShortName", 'chosenCategoryShortName');
       console.log(homeHtml)
       var homeHtmlToInsertIntoMainPage = function(homeHtml) {
         insertHtml("#main-content", homeHtml)
